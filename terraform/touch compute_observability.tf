0d1def2d8357bc838242d078a887d7ef9a3a0432
Sonia Fernanda Calpo Trujillo <scaipotrujillo@gmail.com>
2026-05-06 01:58:33 -0500

    feat:se empaquetaron los Lambdas en ZIP y actualizamos el Terraform

--- terraform/touch compute_observability.tf
+++ terraform/touch compute_observability.tf
@@ -35,15 +35,7 @@ resource "aws_cloudwatch_metric_alarm" "dlq_alarm" {
 }
 
 
-# TRUCO: Creamos un archivo ZIP temporal y vacío para que Terraform no dé error
-data "archive_file" "dummy_lambda" {
-  type        = "zip"
-  output_path = "${path.module}/dummy.zip"
-  source {
-    content  = "exports.handler = async (event) => { return 'Hola, soy de mentira'; };"
-    filename = "index.js"
-  }
-}
+
 
 # Trabajador 1: Upload Lambda
 resource "aws_lambda_function" "upload_lambda" {
@@ -55,8 +47,8 @@ resource "aws_lambda_function" "upload_lambda" {
   timeout       = 30  # Timeout según diagrama
 
   # Usamos el ZIP temporal por ahora
-  filename         = data.archive_file.dummy_lambda.output_path
-  source_code_hash = data.archive_file.dummy_lambda.output_base64sha256
+  filename         = "${path.module}/../src/upload-lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../src/upload-lambda.zip")
 
   # Lo metemos a la VPC (Cuartos secretos y Porteros)
   vpc_config {
@@ -84,8 +76,8 @@ resource "aws_lambda_function" "crop_lambda" {
   memory_size   = 512 # Memoria mayor porque procesar imágenes pesa
   timeout       = 60
 
-  filename         = data.archive_file.dummy_lambda.output_path
-  source_code_hash = data.archive_file.dummy_lambda.output_base64sha256
+  filename         = "${path.module}/../src/crop-lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../src/crop-lambda.zip")
 
   vpc_config {
     subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
